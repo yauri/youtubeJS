7fdcc5d998ed8bd34cb02f64ef2127617875b7f0
yauri <yauri@live.com>
2013-01-22 00:37:47 +0800

    initial commit

---
+++ js/youtubeplayer.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Yauri
+ * Date: 1/21/13
+ * Time: 11:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function YoutubePlayer(width, height){
+    this.width = width || 640;
+    this.height = height || 480;
+
+
+}
+
